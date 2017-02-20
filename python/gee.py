6c585abc4e5dc019694d38a4b45d328e1a4923a8
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-02-20 09:00:13 -0800

    pem in .config/secret

--- python/gee.py
+++ python/gee.py
@@ -1,11 +1,13 @@
 import ee
 import os
 
+PEM='~/.config/secret/privatekey.pem'
+
 def init(user=None):
         sa=os.environ.get('SERVICE_ACCOUNT')
         if sa:
                 print "gee.init[ sevice_account ] {}".format(sa)
-                credentials = ee.ServiceAccountCredentials(sa, 'privatekey.pem')
+                credentials = ee.ServiceAccountCredentials(sa, PEM)
                 ee.Initialize(credentials)
         else:
                 print "gee.init[ user_account ] {}".format(user or '')
